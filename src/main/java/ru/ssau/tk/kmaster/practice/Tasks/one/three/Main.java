6b13e8b741cbad2dc1ebd50d7233cf361eddc462
Krylov <keriesa13@gmail.com>
2021-12-05 22:49:06 +0400

    1.3 готово

---
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/one/three/Main.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.kmaster.practice.Tasks.one.three;
+
+public class Main {
+    Points points_1 = new Points(3.2,1.3,6.2);
+    Points points_2 = new Points(6.9,3.12,9.1);
+    Points points_3 = new Points(5.1,5.6,3.913);
+}
---
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/one/three/Points.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.kmaster.practice.Tasks.one.three;
+
+public class Points {
+    final public double x;
+    final public double y;
+    final public double z;
+
+    public Points(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+    }
+}
