caaed750600b0db64ea98d9c3972435bfcf3a4e7
Krylov <keriesa13@gmail.com>
2021-12-05 22:51:16 +0400

    1.3 готово

--- src/main/java/ru/ssau/tk/kmaster/practice/Tasks/one/three/Main.java
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/one/three/Main.java
@@ -1,7 +1,7 @@
 package ru.ssau.tk.kmaster.practice.Tasks.one.three;
 
 public class Main {
-    Points points_1 = new Points(3.2,1.3,6.2);
-    Points points_2 = new Points(6.9,3.12,9.1);
-    Points points_3 = new Points(5.1,5.6,3.913);
+    Point point_1 = new Point(3.2,1.3,6.2);
+    Point point_2 = new Point(6.9,3.12,9.1);
+    Point point_3 = new Point(5.1,5.6,3.913);
 }
--- src/main/java/ru/ssau/tk/kmaster/practice/Tasks/one/three/Point.java
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/one/three/Point.java
@@ -1,13 +1,13 @@
 package ru.ssau.tk.kmaster.practice.Tasks.one.three;
 
-public class Points {
+public class Point {
     final public double x;
     final public double y;
     final public double z;
 
-    public Points(double x, double y, double z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 }
