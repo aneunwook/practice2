9e365050f521f370d32b471123d9a1932110565c
Krylov <keriesa13@gmail.com>
2021-12-05 22:27:32 +0400

    1.2 готово

---
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/one/two/Main.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.kmaster.practice.Tasks.one.two;
+
+public class Main {
+    Person p_1 = new Person();
+    Person p_2 = new Person(1234);
+    Person p_3 = new Person("Кирилл", "Крылов");
+    Person p_4 = new Person("Кирилл","Крылов", 3616);
+}
