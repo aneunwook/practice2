d30f443111b1b3f2ceb352fdbfb1a6563045e837
Krylov <keriesa13@gmail.com>
2021-12-05 21:09:57 +0400

    First Test!

--- src/main/java/ru/ssau/tk/kmaster/practice/HelloWorld.java
+++ src/main/java/ru/ssau/tk/kmaster/practice/HelloWorld.java
@@ -1,7 +1,12 @@
 package ru.ssau.tk.kmaster.practice;
 
+import org.junit.Test;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static void write() {
         System.out.println("Hello, World!");
     }
 }
---
+++ src/test/java/ru/ssau/tk/kmaster/practice/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.kmaster.practice;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+    private HelloWorld hw = new HelloWorld();
+
+    @Test
+    public void testHelloWorld() {
+        hw.write();
+    }
+}
