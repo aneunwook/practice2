308d5fe6c46de2de7436638f5cb3159f4df6bd98
Krylov <keriesa13@gmail.com>
2021-12-05 20:14:39 +0400

    Ex. 1.1 completed!!!

---
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/one/one/Person.java
@@ -0,0 +1,32 @@
+package ru.ssau.tk.kmaster.practice.Tasks.one.one;
+
+public class Person {
+
+    private String firstName;
+    private String secondName;
+    private int passportId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String firstName) {
+        this.secondName = secondName;
+    }
+
+    public int getPassportId() {
+        return passportId;
+    }
+
+    public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+}
