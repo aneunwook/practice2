6f7dba874e71666a9e790cf2924d537c070e8ab2
Krylov <keriesa13@gmail.com>
2021-12-06 23:42:54 +0400

    1.7 готово

---
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/one/seven/Point.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.kmaster.practice.Tasks.one.seven;
+
+public class Point {
+    final public double x;
+    final public double y;
+    final public double z;
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+}
