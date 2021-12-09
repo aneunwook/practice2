bfed4e630397f0eead02933f42bceac1bc7fef81
Krylov <keriesa13@gmail.com>
2021-12-09 23:02:12 +0400

    Лямбда выражения и метод forEach()

--- src/main/java/ru/ssau/tk/kmaster/practice/StudyPractice/LambdaFunc/SecondExample/Main.java
+++ src/main/java/ru/ssau/tk/kmaster/practice/StudyPractice/LambdaFunc/SecondExample/Main.java
@@ -1,5 +1,6 @@
 package ru.ssau.tk.kmaster.practice.StudyPractice.LambdaFunc.SecondExample;
 
+import java.util.Arrays;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -11,8 +12,18 @@ public class Main {
         Consumer<Object> printer = System.out::println;
 
         double x = sqr.apply(10.);
-        double y = div.apply(3,4);
+        double y = div.apply(3, 4);
         printer.accept(x);
-        printer.accept(y);
+        printer.accept(y );
+        printer.accept("");
+
+        //Добавим метод forEach() типа Iterable
+        //void forEach(Consumer<? super T> action)
+        int[] ints = {1, 2, 3, 4, 5};
+        Arrays.stream(ints).forEach(System.out::println);
+        Arrays.stream(ints).forEach(i -> {
+            System.out.println("i = " + i);
+            System.out.println("i^2 = " + i * i);
+        });
     }
 }
