46398976a320d33902288a556e4791dcc4aae56c
Krylov <keriesa13@gmail.com>
2021-12-09 20:21:21 +0400

    Знакомство с перечислениями

--- src/main/java/ru/ssau/tk/kmaster/practice/StudyPractice/Enums/Level.java
+++ src/main/java/ru/ssau/tk/kmaster/practice/StudyPractice/Enums/Level.java
@@ -1,4 +1,4 @@
-package ru.ssau.tk.kmaster.practice.Enums;
+package ru.ssau.tk.kmaster.practice.StudyPractice.Enums;
 
 public enum Level {
     BEGGINER("green"),
--- src/main/java/ru/ssau/tk/kmaster/practice/StudyPractice/Enums/Test.java
+++ src/main/java/ru/ssau/tk/kmaster/practice/StudyPractice/Enums/Test.java
@@ -1,13 +1,20 @@
-package ru.ssau.tk.kmaster.practice.Enums;
+package ru.ssau.tk.kmaster.practice.StudyPractice.Enums;
 
 import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
+        String color = "white";
+        Level lvl = Level.EXPERT;
+        if (lvl.getColor().equals(color)) {
+            System.out.println(lvl.name() + " is " + lvl.getColor());
+        }
         System.out.println(Arrays.toString(Level.values()));
         System.out.println(Level.BEGGINER.getColor());
         Level.BEGGINER.setColor("red");
         System.out.println(Level.BEGGINER.getColor());
         System.out.println(Level.BEGGINER.toString());
+        System.out.println(lvl.getColor());
+        System.out.println(lvl.toString());
     }
 }
