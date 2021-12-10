f26c790d29ebf2afc35eb84c99831407b30e1847
Krylov <keriesa13@gmail.com>
2021-12-10 20:42:45 +0400

    Реализация try-with-resources и потоки ввода и вывода

--- src/main/java/ru/ssau/tk/kmaster/practice/StudyPractice/TryWithResources/Main.java
+++ src/main/java/ru/ssau/tk/kmaster/practice/StudyPractice/TryWithResources/Main.java
@@ -1,12 +1,11 @@
 package ru.ssau.tk.kmaster.practice.StudyPractice.TryWithResources;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         try (FileInputStream fis = new FileInputStream("C:/Users/monster/Desktop/уник/Прога/Оксимирон.txt");
              FileOutputStream fos = new FileOutputStream("C:/Users/monster/Desktop/уник/Прога/WhoKilledMark.txt");
              ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
@@ -17,6 +16,31 @@ public class Main {
             baos.writeTo(fos);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        try (FileInputStream fis = new FileInputStream("C:/Users/monster/Desktop/уник/Прога/4.txt");
+             InputStreamReader isr = new InputStreamReader(fis, "UTF-8")) {
+            int b;
+            while ((b = isr.read()) != -1) {
+                System.out.print((char) b);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        String str = "AFBU@BPQGBQOGROPPONDGPWJNVPWU)@RU*JVN*@284h1nPNFWSG91mfpsdm)@!2131";
+        String nL = "\n";
+        FileWriter fw = null;
+        File file = new File("C:/Users/monster/Desktop/уник/Прога/5fake.txt");
+
+        try {
+            fw = new FileWriter(file, true);
+            fw.write(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            fw.close();
         }
     }
 }
