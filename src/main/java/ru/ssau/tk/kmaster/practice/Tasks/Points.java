1d41c31af46708efc2637190b44744857a60d141
Krylov <keriesa13@gmail.com>
2021-12-22 20:21:23 +0400

    1.7

--- src/main/java/ru/ssau/tk/kmaster/practice/Tasks/Points.java
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/Points.java
@@ -18,12 +18,20 @@ public class Points {
         return new Point(first.x / second.x, first.y / second.y, first.z / second.z);
     }
 
-    static public Point enlarge(Point point, double num){
-        return new Point(point.x * num, point.y * num , point.z * num);
+    static public Point enlarge(Point point, double num) {
+        return new Point(point.x * num, point.y * num, point.z * num);
     }
 
-    static  public  double length(Point p){
+    static public double length(Point p) {
         return p.length();
+    }
+
+    static public Point opposite(Point p) {
+        return new Point(-p.x, -p.y, -p.z);
+    }
+
+    static public Point inverse(Point p) {
+        return new Point(1 / p.x, 1 / p.y, 1 / p.z);
     }
 
     private Points() {
--- src/main/java/ru/ssau/tk/kmaster/practice/Tasks/Test.java
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/Test.java
@@ -2,7 +2,8 @@ package ru.ssau.tk.kmaster.practice.Tasks;
 
 public class Test {
     public static void main(String[] args) {
-        Point point = new Point(1.3,6.2,9.1);
-        System.out.println(Points.length(point));
+        Point point = new Point(1.4, 8.1, 7);
+        Point rez_1 = Points.opposite(point);
+        Point rez_2 = Points.inverse(point);
     }
 }
