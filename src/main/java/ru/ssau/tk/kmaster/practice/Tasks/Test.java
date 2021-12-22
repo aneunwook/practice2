b68c9ebe3129b431888b4db8e2cc8b3348a54db5
Krylov <keriesa13@gmail.com>
2021-12-22 19:55:10 +0400

    1.1

---
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/Test.java
@@ -0,0 +1,30 @@
+package ru.ssau.tk.kmaster.practice.Tasks;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Person first = new Person();
+        Person second = new Person();
+        Person third = new Person();
+
+        first.setFirstName("Сергей");
+        second.setFirstName("Кирилл");
+        third.setFirstName("Алексей");
+        first.setSecondName("Кривцов");
+        second.setSecondName("Крылов");
+        third.setSecondName("Яров");
+        first.setPassportId(2143);
+        second.setPassportId(9213);
+        third.setPassportId(1932);
+
+        String f_1 = first.getFirstName();
+        String s_1 = first.getSecondName();
+        int p_1 = first.getPassportId();
+        String f_2 = second.getFirstName();
+        String s_2 = second.getSecondName();
+        int p_2 = second.getPassportId();
+        String f_3 = third.getFirstName();
+        String s_3 = third.getSecondName();
+        int p_3 = third.getPassportId();
+    }
+}
