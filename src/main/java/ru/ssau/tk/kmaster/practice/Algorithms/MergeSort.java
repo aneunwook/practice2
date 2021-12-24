1f54dbdf385212a7c9a3f62955a82a9267332857
Krylov <keriesa13@gmail.com>
2021-12-24 19:57:41 +0400

    Сортировка слиянием

---
+++ src/main/java/ru/ssau/tk/kmaster/practice/Algorithms/MergeSort.java
@@ -0,0 +1,67 @@
+package ru.ssau.tk.kmaster.practice.Algorithms;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{58, 29, 85, 41, 23, 35, 61, 42, 75, 24, 47, 55, 63};
+        System.out.println("Source array: " + arrayToString(array) + "\n");
+        mergeSort(array);
+        System.out.println("\nSort array: " + arrayToString(array));
+    }
+
+    private static String arrayToString(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(arr[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private static void merge(int[] src1, int src1Start, int[] src2, int src2Start,
+                              int[] dest, int destStart, int size) {
+        int index1 = src1Start;
+        int index2 = src2Start;
+
+        int src1End = Math.min(src1Start + size, src1.length);
+        int src2End = Math.min(src2Start + size, src2.length);
+
+        int iterationCount = src1End - src1Start + src2End - src2Start;
+
+        for (int i = destStart; i < destStart + iterationCount; i++) {
+            if (index1 < src1End && (index2 >= src2End || src1[index1] < src2[index2])) {
+                dest[i] = src1[index1];
+                index1++;
+            } else {
+                dest[i] = src2[index2];
+                index2++;
+            }
+        }
+    }
+
+    private static void mergeSort(int[] array) {
+        int[] tmp;
+        int[] currentSrc = array;
+        int[] currentDest = new int[array.length];
+
+        int size = 1;
+
+        while (size < array.length) {
+            for (int i = 0; i < array.length; i += 2 * size) {
+                merge(currentSrc, i, currentSrc, i + size, currentDest, i, size);
+            }
+
+            tmp = currentSrc;
+            currentSrc = currentDest;
+            currentDest = tmp;
+
+            size *= 2;
+
+            System.out.println(arrayToString(currentSrc));
+        }
+    }
+}
