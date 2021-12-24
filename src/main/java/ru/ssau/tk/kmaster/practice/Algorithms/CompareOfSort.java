cfd168ab0c1118624e5300b69c8a9d5a3af8087e
Krylov <keriesa13@gmail.com>
2021-12-24 19:08:33 +0400

    Сравнение быстрой и пузырьковой сортировки по времени

---
+++ src/main/java/ru/ssau/tk/kmaster/practice/Algorithms/CompareOfSort.java
@@ -0,0 +1,90 @@
+package ru.ssau.tk.kmaster.practice.Algorithms;
+
+import java.util.Random;
+
+public class CompareOfSort {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static void quickSort(int[] arr, int from, int to) {
+
+        if (from < to) {
+            int divideIndex = partition(arr, from, to);
+            quickSort(arr, from, divideIndex - 1);
+            quickSort(arr, divideIndex, to);
+        }
+    }
+
+    private static int partition(int[] arr, int from, int to) {
+        int rightIndex = to;
+        int leftIndex = from;
+
+        int pivot = arr[from];
+
+        while (leftIndex <= rightIndex) {
+
+            while (arr[leftIndex] < pivot) {
+                leftIndex++;
+            }
+
+            while (arr[rightIndex] > pivot) {
+                rightIndex--;
+            }
+
+            if (leftIndex <= rightIndex) {
+                swap(arr, rightIndex, leftIndex);
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+        return leftIndex;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int tmp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tmp;
+    }
+
+    private static void bubbleSort(int[] arr) {
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i - 1] > arr[i]) {
+                    swap(arr, i - 1, i);
+                    sorted = false;
+                }
+            }
+        }
+    }
+
+    private static void measureTime(Runnable task) {
+
+        long startTime = System.currentTimeMillis();
+        task.run();
+        long elapsed = System.currentTimeMillis() - startTime;
+        System.out.println("Затраченное время: " + elapsed + " ms");
+    }
+
+    private static void test() {
+        int testLen = 100_000;
+
+        int[] arr1 = new int[testLen];
+        int[] arr2 = new int[testLen];
+
+        Random r = new Random();
+
+        for (int i = 0; i < testLen; i++) {
+            arr1[i] = arr2[i] = r.nextInt(10000);
+        }
+
+        System.out.println("Быстрая сортировка");
+        measureTime(() -> quickSort(arr1, 0, testLen - 1));
+
+        System.out.println("Пузырьковая сортировка");
+        measureTime(() -> bubbleSort(arr2));
+    }
+}
