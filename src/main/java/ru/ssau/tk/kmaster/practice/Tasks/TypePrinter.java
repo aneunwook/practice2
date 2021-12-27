83aeb1e57f842b8a7750c006767f47b616c330f2
Krylov <keriesa13@gmail.com>
2021-12-27 17:50:20 +0400

    1.19

---
+++ src/main/java/ru/ssau/tk/kmaster/practice/Tasks/TypePrinter.java
@@ -0,0 +1,60 @@
+package ru.ssau.tk.kmaster.practice.Tasks;
+
+import java.util.Objects;
+
+public class TypePrinter {
+
+    public static void main(String[] args) {
+        byte number = 0;
+        printType(number);
+        printType((char) number);
+        printType((short) number);
+        printType((int) number);
+        printType((long) number);
+        printType((float) number);
+        printType((double) number);
+        printType(true);
+        printType(new Person());
+        printType(null);
+    }
+
+    public static void printType(byte number) {
+        System.out.println("Byte " + number);
+    }
+
+    public static void printType(char number) {
+        System.out.println("Char " + number);
+    }
+
+    public static void printType(short number) {
+        System.out.println("Short " + number);
+    }
+
+    public static void printType(int number) {
+        System.out.println("Int " + number);
+    }
+
+    public static void printType(long number) {
+        System.out.println("Long " + number);
+    }
+
+    public static void printType(float number) {
+        System.out.println("Float " + number);
+    }
+
+    public static void printType(double number) {
+        System.out.println("Double " + number);
+    }
+
+    public static void printType(boolean number) {
+        System.out.println("Boolean " + number);
+    }
+
+    public static void printType(Object number) {
+        if (number == null) {
+            System.out.println("Null");
+        } else {
+            System.out.println(Objects.requireNonNull(number).getClass());
+        }
+    }
+}
